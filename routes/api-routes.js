5865c621d2ec6e3a7211f73a051bd3f3d21b27d7
skuttenkuler <samkiddmusic@gmail.com>
2020-02-01 19:29:12 -0800

    html routes for all html

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,6 +12,7 @@ module.exports = (app) => {
             } else {
                 res.json(workouts)
             }
-        })
-    })
+        });
+    });
+    
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,2 +1,18 @@
-//need route for new excersice form
-//need route to continue excersise
+const db = require('../models')
+const path = require('path');
+
+module.exports = (app) => {
+//index
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+//exercise
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+//stats
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+}
+
