78ad3650a8f4ba9a0c6c9b95c6f0211dda3f8c53
skuttenkuler <samkiddmusic@gmail.com>
2020-02-01 16:21:17 -0800

    server.js, node server works and server is listening

---
+++ server.js
@@ -0,0 +1,24 @@
+//install packages
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+//set port
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//use logger
+app.use(logger("dev"));
+
+//parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//use static files
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", {useNewUrlParser: true});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+})
