04d98d5ae3eaa17ccfbb2870eeecb7af63833df8
skuttenkuler <samkiddmusic@gmail.com>
2020-02-04 23:04:43 -0800

    models update

--- models/workout.js
+++ models/workout.js
@@ -8,12 +8,21 @@ const WorkoutSchema = new Schema({
         type: Date,
         default: Date.now()
     },
-    excercises: [],
+    excercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ],
     totalDuration: {
         type: Number,
         default: 0
     } 
 });
+//change totalDuration everytime we add a new exercise
+WorkoutSchema.post('findOneAndUpdate', (workouts) => {
+    //console.log(this)
+})
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
